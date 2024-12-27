1ea42023d269a2cb5cf91dcde0cae64e00767f0c
yashkurkure <yash.kurkure1903@gmail.com>
2024-12-26 23:29:44 -0500

    oneko

---
+++ README.md
@@ -0,0 +1,8 @@
+# Change Log
+- 12/26/24 Added oneko
+
+
+# Acknowledgements
+https://maia.crimew.gay/
+https://outerproduct.net/
+https://github.com/adryd325/oneko.js
--- script.js
+++
@@ -1,9 +0,0 @@
-const catImg = document.getElementById('cat-img');
-
-document.addEventListener('mousemove', (event) => {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-
-    // Use transform for smoother movement
-    catImg.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
-});
